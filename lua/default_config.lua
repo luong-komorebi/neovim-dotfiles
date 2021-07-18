c4b6c45862b8d64de958d54a7700f00c9fde605f
Luong Vo <vo.tran.thanh.luong@gmail.com>
2021-07-18 19:28:05 +0700

    Add some more sane settings

--- lua/default_config.lua
+++ lua/default_config.lua
@@ -24,6 +24,7 @@ O = {
     hidden = true, -- required to keep multiple buffers and open multiple buffers
     hlsearch = true, -- highlight all matches on previous search pattern
     ignorecase = true, -- ignore case in search patterns
+    inccommand = split, -- live substitution using vim's :substitue
     mouse = "a", -- allow the mouse to be used in neovim
     pumheight = 10, -- pop up menu height
     showmode = false, -- we don't need to see things like -- INSERT -- anymore
