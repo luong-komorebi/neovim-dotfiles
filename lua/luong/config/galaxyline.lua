f47250f23d24f3665d9eb51c8d6df57f5ac55020
Luong Vo <vo.tran.thanh.luong@gmail.com>
2021-07-27 15:03:40 +0700

    minor enhancement to bufferline and galaxy line

--- lua/luong/config/galaxyline.lua
+++ lua/luong/config/galaxyline.lua
@@ -1,15 +1,15 @@
 -- galaxy line depends on colors from tokyonight, so to prevent this from failure in first load:
-local status_ok, _ = pcall(require, "tokyonight")
+local status_ok, _ = pcall(require, 'tokyonight')
 if not status_ok then
   return
 end
 
-local gl = require('galaxyline')
-local condition = require('galaxyline.condition')
+local gl = require 'galaxyline'
+local condition = require 'galaxyline.condition'
 local gls = gl.section
 local c = O.pallete
 
-gl.short_line_list = { "qf", "vista_kind", "terminal", "packer", "CHADTree" }
+gl.short_line_list = { 'qf', 'vista_kind', 'terminal', 'packer', 'CHADTree' }
 
 -- Colors from tokyonight theme
 local colors = {
@@ -28,8 +28,10 @@ local colors = {
 
 table.insert(gls.left, {
   FirstElement = {
-    provider = function() return '▋' end,
-    highlight = { colors.cyan, colors.section_bg }
+    provider = function()
+      return '▋'
+    end,
+    highlight = { colors.cyan, colors.section_bg },
   },
 })
 
@@ -43,29 +45,29 @@ table.insert(gls.left, {
         c = 'CMD',
         V = 'VISUAL',
         [''] = 'VISUAL',
-        v ='VISUAL',
+        v = 'VISUAL',
         ['r?'] = ':CONFIRM',
         rm = '--MORE',
-        R  = 'REPLACE',
+        R = 'REPLACE',
         Rv = 'VIRTUAL',
-        s  = 'SELECT',
-        S  = 'SELECT',
-        ['r']  = 'HIT-ENTER',
+        s = 'SELECT',
+        S = 'SELECT',
+        ['r'] = 'HIT-ENTER',
         [''] = 'SELECT',
-        t  = 'TERMINAL',
-        ['!']  = 'SHELL',
+        t = 'TERMINAL',
+        ['!'] = 'SHELL',
       }
       local mode_color = {
         n = colors.blue,
         i = colors.green,
         v = colors.purple,
-        [""] = colors.purple,
+        [''] = colors.purple,
         V = colors.purple,
         c = colors.magenta,
         no = colors.blue,
         s = colors.orange,
         S = colors.orange,
-        [""] = colors.orange,
+        [''] = colors.orange,
         ic = colors.yellow,
         R = colors.red,
         Rv = colors.red,
@@ -73,19 +75,19 @@ table.insert(gls.left, {
         ce = colors.blue,
         r = colors.cyan,
         rm = colors.cyan,
-        ["r?"] = colors.cyan,
-        ["!"] = colors.blue,
+        ['r?'] = colors.cyan,
+        ['!'] = colors.blue,
         t = colors.blue,
       }
-      vim.api.nvim_command("hi GalaxyViMode guifg=" .. mode_color[vim.fn.mode()])
+      vim.api.nvim_command('hi GalaxyViMode guifg=' .. mode_color[vim.fn.mode()])
       local alias_mode = alias[vim.fn.mode()]
       if alias_mode == nil then
         alias_mode = vim.fn.mode()
       end
-      return alias_mode..' '
+      return alias_mode .. ' '
     end,
     highlight = { colors.bg, colors.bg },
-    separator = "  ",
+    separator = '  ',
     separator_highlight = { colors.bg, colors.section_bg },
   },
 })
@@ -93,10 +95,10 @@ table.insert(gls.left, {
 table.insert(gls.left, {
   GitIcon = {
     provider = function()
-      return "  "
+      return '  '
     end,
     condition = condition.check_git_workspace,
-    separator = " ",
+    separator = ' ',
     separator_highlight = { colors.bg, colors.section_bg },
     highlight = { colors.orange, colors.section_bg },
   },
@@ -105,12 +107,12 @@ table.insert(gls.left, {
 table.insert(gls.left, {
   GitBranch = {
     provider = function()
-      local vcs = require('galaxyline.provider_vcs')
+      local vcs = require 'galaxyline.provider_vcs'
       local branch_name = vcs.get_git_branch()
-      if (string.len(branch_name) > 28) then
-        return string.sub(branch_name, 1, 25).."..."
+      if string.len(branch_name) > 28 then
+        return string.sub(branch_name, 1, 25) .. '...'
       end
-      return branch_name .. " "
+      return branch_name .. ' '
     end,
     condition = condition.check_git_workspace,
     highlight = { colors.fg, colors.section_bg },
@@ -119,27 +121,27 @@ table.insert(gls.left, {
 
 table.insert(gls.left, {
   DiffAdd = {
-    provider = "DiffAdd",
+    provider = 'DiffAdd',
     condition = condition.hide_in_width,
-    icon = "  ",
+    icon = '  ',
     highlight = { colors.green, colors.section_bg },
   },
 })
 
 table.insert(gls.left, {
   DiffModified = {
-    provider = "DiffModified",
+    provider = 'DiffModified',
     condition = condition.hide_in_width,
-    icon = " 柳",
+    icon = ' 柳',
     highlight = { colors.blue, colors.section_bg },
   },
 })
 
 table.insert(gls.left, {
   DiffRemove = {
-    provider = "DiffRemove",
+    provider = 'DiffRemove',
     condition = condition.hide_in_width,
-    icon = "  ",
+    icon = '  ',
     highlight = { colors.red, colors.section_bg },
   },
 })
@@ -147,29 +149,29 @@ table.insert(gls.left, {
 table.insert(gls.left, {
   Filler = {
     provider = function()
-      return " "
+      return ' '
     end,
     highlight = { colors.grey, colors.section_bg },
   },
 })
 -- get output from shell command
 function os.capture(cmd, raw)
-  local f = assert(io.popen(cmd, "r"))
-  local s = assert(f:read "*a")
+  local f = assert(io.popen(cmd, 'r'))
+  local s = assert(f:read '*a')
   f:close()
   if raw then
     return s
   end
-  s = string.gsub(s, "^%s+", "")
-  s = string.gsub(s, "%s+$", "")
-  s = string.gsub(s, "[\n\r]+", " ")
+  s = string.gsub(s, '^%s+', '')
+  s = string.gsub(s, '%s+$', '')
+  s = string.gsub(s, '[\n\r]+', ' ')
   return s
 end
 -- cleanup virtual env
 local function env_cleanup(venv)
-  if string.find(venv, "/") then
+  if string.find(venv, '/') then
     local final_venv = venv
-    for w in venv:gmatch "([^/]+)" do
+    for w in venv:gmatch '([^/]+)' do
       final_venv = w
     end
     venv = final_venv
@@ -177,82 +179,82 @@ local function env_cleanup(venv)
   return venv
 end
 local PythonEnv = function()
-  if vim.bo.filetype == "python" then
-    local venv = os.getenv "CONDA_DEFAULT_ENV"
+  if vim.bo.filetype == 'python' then
+    local venv = os.getenv 'CONDA_DEFAULT_ENV'
     if venv ~= nil then
-      return "  (" .. env_cleanup(venv) .. ")"
+      return '  (' .. env_cleanup(venv) .. ')'
     end
-    venv = os.getenv "VIRTUAL_ENV"
+    venv = os.getenv 'VIRTUAL_ENV'
     if venv ~= nil then
-      return "  (" .. env_cleanup(venv) .. ")"
+      return '  (' .. env_cleanup(venv) .. ')'
     end
-    return ""
+    return ''
   end
-  return ""
+  return ''
 end
 table.insert(gls.left, {
   VirtualEnv = {
     provider = PythonEnv,
-    event = "BufEnter",
+    event = 'BufEnter',
     highlight = { colors.green, colors.section_bg },
   },
 })
 
 table.insert(gls.right, {
   DiagnosticError = {
-    provider = "DiagnosticError",
-    icon = "  ",
+    provider = 'DiagnosticError',
+    icon = '  ',
     highlight = { colors.red, colors.section_bg },
   },
 })
 table.insert(gls.right, {
   DiagnosticWarn = {
-    provider = "DiagnosticWarn",
-    icon = "  ",
+    provider = 'DiagnosticWarn',
+    icon = '  ',
     highlight = { colors.orange, colors.section_bg },
   },
 })
 
 table.insert(gls.right, {
   DiagnosticInfo = {
-    provider = "DiagnosticInfo",
-    icon = "  ",
+    provider = 'DiagnosticInfo',
+    icon = '  ',
     highlight = { colors.yellow, colors.section_bg },
   },
 })
 
 table.insert(gls.right, {
   DiagnosticHint = {
-    provider = "DiagnosticHint",
-    icon = "  ",
+    provider = 'DiagnosticHint',
+    icon = '  ',
     highlight = { colors.blue, colors.section_bg },
   },
 })
 
 local get_lsp_client = function(msg)
-  msg = msg or "LSP Inactive"
-  local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+  msg = msg or 'LSP Inactive'
+  local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
   local clients = vim.lsp.get_active_clients()
   if next(clients) == nil then
     return msg
   end
-  local lsps = ""
+  local lsps = ''
   for _, client in ipairs(clients) do
     local filetypes = client.config.filetypes
     if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
       -- print(client.name)
-      if lsps == "" then
+      if lsps == '' then
         -- print("first", lsps)
         lsps = client.name
       else
         if not string.find(lsps, client.name) then
-          lsps = lsps .. "," .. client.name
+          lsps = lsps .. ',' .. client.name
         end
         -- print("more", lsps)
       end
     end
   end
-  if lsps == "" then
+  if lsps == '' then
     return msg
   else
     return lsps
@@ -262,22 +264,22 @@ end
 table.insert(gls.right, {
   ShowLspClient = {
     provider = get_lsp_client,
-    condition = function()
-      local tbl = { ["dashboard"] = true, [" "] = true }
+    condition = condition.hide_in_width or function()
+      local tbl = { ['dashboard'] = true, [' '] = true }
       if tbl[vim.bo.filetype] then
         return false
       end
       return true
     end,
-    icon = "  ",
+    icon = '  ',
     highlight = { colors.grey, colors.section_bg },
   },
 })
 
 table.insert(gls.right, {
   LineInfo = {
-    provider = "LineColumn",
-    separator = " ",
+    provider = 'LineColumn',
+    separator = ' ',
     separator_highlight = { colors.bg, colors.section_bg },
     highlight = { colors.grey, colors.section_bg },
   },
@@ -285,8 +287,8 @@ table.insert(gls.right, {
 
 table.insert(gls.right, {
   PerCent = {
-    provider = "LinePercent",
-    separator = " ",
+    provider = 'LinePercent',
+    separator = ' ',
     separator_highlight = { colors.bg, colors.section_bg },
     highlight = { colors.grey, colors.section_bg },
   },
@@ -295,17 +297,17 @@ table.insert(gls.right, {
 table.insert(gls.right, {
   FileIcon = {
     provider = 'FileIcon',
-    separator = " ",
+    separator = ' ',
     separator_highlight = { colors.bg, colors.section_bg },
     condition = condition.buffer_not_empty,
     highlight = { require('galaxyline.provider_fileinfo').get_file_icon_color, colors.section_bg },
-   },
+  },
 })
 
 table.insert(gls.right, {
   BufferType = {
-    provider = "FileTypeName",
-    condition = condition.hide_in_width and condition.buffer_not_empty,
+    provider = 'FileTypeName',
+    condition = condition.hide_in_width or condition.buffer_not_empty,
     separator_highlight = { colors.bg, colors.section_bg },
     highlight = { colors.grey, colors.section_bg },
   },
@@ -313,9 +315,9 @@ table.insert(gls.right, {
 
 table.insert(gls.right, {
   FileEncode = {
-    provider = "FileEncode",
+    provider = 'FileEncode',
     condition = condition.hide_in_width,
-    separator = " ",
+    separator = ' ',
     separator_highlight = { colors.bg, colors.section_bg },
     highlight = { colors.grey, colors.section_bg },
   },
@@ -323,7 +325,7 @@ table.insert(gls.right, {
 
 table.insert(gls.short_line_left, {
   SFileName = {
-    provider = "SFileName",
+    provider = 'SFileName',
     condition = condition.buffer_not_empty,
     highlight = { colors.fg, colors.section_bg },
   },
@@ -331,7 +333,7 @@ table.insert(gls.short_line_left, {
 
 table.insert(gls.short_line_right, {
   BufferIcon = {
-    provider= 'BufferIcon',
+    provider = 'BufferIcon',
     highlight = { colors.fg, colors.section_bg },
-  }
+  },
 })
