0bb69c347375201305ea0ea3008b702d302b74b7
Luong Vo <vo.tran.thanh.luong@gmail.com>
2021-07-30 08:35:51 +0700

    auto install packages on second load
    Signed-off-by: Luong Vo <vo.tran.thanh.luong@gmail.com>

--- lua/luong/colorscheme.lua
+++ lua/luong/colorscheme.lua
@@ -1,7 +1,14 @@
 -- on first load the theme may not be available yet
 local status_ok, _ = pcall(require, "tokyonight")
 if not status_ok then
-  return
+  -- this signals that no packages are installed
+  -- so we can either install if packer is there or just return
+  local packer_loaded, _ = pcall(require, "packer")
+  if packer_loaded then
+    require("packer").install()
+  else
+    return
+  end
 end
 
 vim.g.tokyonight_style = "storm"
