846b6625403a14d0ec29c680f88bb860efd84a5a
Luong Vo <vo.tran.thanh.luong@gmail.com>
2021-09-05 21:07:11 +0700

    reconfigure some plugins and move some to lua instead of vim

--- lua/luong/hop.lua
+++ lua/luong/hop.lua
@@ -1,3 +1,3 @@
 require("hop").setup()
 vim.api.nvim_set_keymap("n", "<Leader><Tab>", "<cmd>lua require'hop'.hint_words()<cr>", {})
-vim.api.nvim_set_keymap("n", "ss", "<cmd>lua require'hop'.hint_char2()<cr>", {})
+vim.api.nvim_set_keymap("n", "<Leader><Leader>s", "<cmd>lua require'hop'.hint_char2()<cr>", {})
--- lua/luong/vim-visual-multi.lua
+++ lua/luong/vim-visual-multi.lua
@@ -1,1 +1,6 @@
 vim.g.VM_mouse_mappings = 1
+vim.g.VM_default_mappings = 0
+vim.g["VM_maps['Find Under']"] = "<C-n>"
+vim.g["VM_maps['Find Subword Under']"] = "<C-n>"
+vim.g["VM_maps['Select All']"] = [[\\A]]
+vim.g["VM_maps['Add Cursor At Pos']"] = [[\\\]]
