2bddc0a27e1d86f855bec8cb8852d04722ae554a
Luong Vo <vo.tran.thanh.luong@gmail.com>
2021-09-13 08:28:23 +0700

    do not automatically install package in colorscheme

--- lua/luong/colorscheme.lua
+++ lua/luong/colorscheme.lua
@@ -1,14 +1,7 @@
 -- on first load the theme may not be available yet
 local status_ok, _ = pcall(require, "tokyonight")
 if not status_ok then
-  -- this signals that no packages are installed
-  -- so we can either install if packer is there or just return
-  local packer_loaded, _ = pcall(require, "packer")
-  if packer_loaded then
-    require("packer").install()
-  else
-    return
-  end
+  return
 end
 
 vim.g.tokyonight_style = "storm"
