d6b9ba1d5e192105748a836b31716c961a24bd52
Luong Vo <vo.tran.thanh.luong@gmail.com>
2021-09-25 21:33:20 +0700

    refactor and update nvim

--- lua/config/lang_config.lua
+++ lua/config/lang_config.lua
@@ -61,12 +61,6 @@ O.lang = {
         -- on_attach = common_on_attach,
         settings = {
           Lua = {
-            runtime = {
-              -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-              version = "LuaJIT",
-              -- Setup your lua path
-              path = vim.split(package.path, ";"),
-            },
             diagnostics = {
               -- Get the language server to recognize the `vim` global
               globals = { "vim", "O" },
--- lua/luong/gitsigns.lua
+++ lua/luong/gitsigns.lua
@@ -35,5 +35,16 @@ gitsigns.setup {
       numhl = "GitSignsChangeNr",
       linehl = "GitSignsChangeLn",
     },
+    numhl = false,
+    linehl = false,
+    keymaps = {
+      -- Default keymap options
+      noremap = true,
+      buffer = true,
+    },
+    watch_gitdir = { interval = 1000 },
+    sign_priority = 6,
+    update_debounce = 200,
+    status_formatter = nil, -- Use default
   },
 }
